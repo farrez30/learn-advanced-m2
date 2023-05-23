f2097a287b7fb09fb422ced50d36710f20ab6da2
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-05-23 17:54:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import LearnHOC from "./pages/LearnHOC";
+// import LearnHOC from "./pages/LearnHOC";
 import Testing from "./pages/Testing";
 import Home from "./pages/Home";
 
 function App() {
+  // const Button = (props) => <button style={props.style}>Click me</button>;
+  // const Text = (props) => <p style={props.style}>Testing</p>;
 
-  const Button = (props) => <button style={props.style}>Click me</button>;
-  const Text = (props) => <p style={props.style}>Testing</p>;
-
-  const StyleButton = LearnHOC(Button);
-  const StyleText = LearnHOC(Text);
+  // const StyleButton = LearnHOC(Button);
+  // const StyleText = LearnHOC(Text);
 
   return (
     <div>
